b4f60532539e2b4f7eecbe6a6bbf771645586f35
divyanjalibagga <divyanjalibagga@gmail.com>
2026-02-14 11:38:32 +0000

    introduction to cpp

---
+++ code.cpp
@@ -0,0 +1,56 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello world";
+    return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+
+return 0;
+} boilerplate code.
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello world " << "guddu" << endl;
+    //cout <<"divyanjali bagga\n from guddu" << endl;
+    return 0;
+}
+*/
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     cout << "divyanjali\n" << "bagga\n";
+//     return 0;
+
+// }
+
+
+//Variables. containers to sore data.
+// age(variable) = 25
+// data can be number, character
+//always starts from '_' or (a-z),(A-Z)
+//variable names are identifier
+// data type: int, float, char, bool, double. 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 25;
+    cout << "age"
+    return 0;
+}
